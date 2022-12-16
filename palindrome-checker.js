b384e4d65fd9835c47e975f51e07e45f935993e2
jugosack <jugoslav_ackoski@yahoo.com>
2022-12-16 16:12:51 +0100

    Add solution_1 for palindrome

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -1,3 +1,22 @@
 module.exports = function (str) {
   // write your code here
-}
+  /*First, we remove any non-alphabet character using regex and convert
+    convert the strings to lowercase. */
+    str = str.replace(/[\W_]/g, "").toLowerCase();
+
+    // convert string to an array
+    const arrStr = str.split('');
+
+    // reverse the array 
+    const revArrStr = arrStr.reverse();
+
+    // convert array to string
+    const revStr = revArrStr.join('');
+
+    if(str == revStr) {
+        return true;
+    }
+    else {
+        return false;
+    }
+  }
