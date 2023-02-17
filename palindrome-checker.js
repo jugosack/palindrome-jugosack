01996cfb4d386e95e7660d0a3b286c261fdcf010
jugosack <jugoslav_ackoski@yahoo.com>
2023-02-17 09:43:02 +0200

    Add mob programming challenges

--- palindrome-checker.js
+++ palindrome-checker.js
@@ -2,21 +2,9 @@ module.exports = function (str) {
   // write your code here
   /*First, we remove any non-alphabet character using regex and convert
     convert the strings to lowercase. */
-    str = str.replace(/[\W_]/g, "").toLowerCase();
+    let reverse=str.toLowerCase().replaceAll(/[.,\/#!$%\^&\*;:{}=\-_` ~()]/g, "").split("").reverse().join()
 
-    // convert string to an array
-    const arrStr = str.split('');
-
-    // reverse the array 
-    const revArrStr = arrStr.reverse();
-
-    // convert array to string
-    const revStr = revArrStr.join('');
-
-    if(str == revStr) {
-        return true;
-    }
-    else {
-        return false;
-    }
+    let strRev=str.toLowerCase().replaceAll(/[.,\/#!$%\^&\*;:{}=\-_` ~()]/g, "").split("").join()
+    return strRev===reverse
+   
   }
